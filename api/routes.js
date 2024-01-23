3847d925043c8f059ad60e5cfd1d829bfa2af757
Georgi <gdaleev2346@gmail.com>
2024-01-23 11:57:36 +0200

    Added search

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
-const router = require("express").Router();
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
 
 router.get("/", (req, res) => {
   res.render("main");
@@ -15,25 +16,41 @@ router.get("/create", (req, res) => {
 });
 
 router.get("/search", (req, res) => {
-  res.render("search");
-}); 
+  const databasePath = path.join(__dirname, "..", "config", "database.json");
+
+  const moviesData = JSON.parse(fs.readFileSync(databasePath, "utf-8"));
+
+  res.render("search", { movies: moviesData });
+});
+
+router.post('/search', (req, res) => {
+  const {title, genre, year } = req.body
+  const databasePath = path.join(__dirname, "..", "config", "database.json");
+  const movies = JSON.parse(fs.readFileSync(databasePath, "utf-8"));
+  const searchResults = movies.filter(movie =>
+    (!title || movie.title.toLowerCase().includes(title.toLowerCase())) &&
+    (!genre || movie.genre.toLowerCase().includes(genre.toLowerCase())) &&
+    (!year || movie.year === year)
+  );
+
+  // Render the search view with the search results
+  res.render('search', { movies: searchResults, searchParams: { title, genre, year } });
+})
 
 router.get("/details/:id", (req, res) => {
   let movieId = req.params.id;
-  movieId = movieId.replace(':', '');
+  movieId = movieId.replace(":", "");
 
-  const databasePath = path.join(__dirname, "..", "config", "database.json")
-  const moviesData = JSON.parse(fs.readFileSync(databasePath, 'utf-8'))
+  const databasePath = path.join(__dirname, "..", "config", "database.json");
+  const moviesData = JSON.parse(fs.readFileSync(databasePath, "utf-8"));
 
-  const movie  = moviesData.find(movie => movie.id == movieId)
+  const movie = moviesData.find((movie) => movie.id == movieId);
 
-  res.render("details", { movie })
-})
-
-router.use((req, res, next) => {
-  res.status(404).render("404"); 
+  res.render("details", { movie });
 });
 
-
+router.use((req, res, next) => {
+  res.status(404).render("404");
+});
 
 module.exports = router;
