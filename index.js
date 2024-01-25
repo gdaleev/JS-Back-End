6e5aee9b9bc8bb3ec993cdabfc169f07e68f4f15
Georgi <gdaleev2346@gmail.com>
2024-01-25 11:34:10 +0200

    Connected to the database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const expressConfig = require("./expressConfig");
 const hbsConfig = require("./hbsConfig");
-
+const connectDB = require("./connectDB")
 const port = 3000;
+
+connectDB()
 expressConfig(port);
 hbsConfig(port);
